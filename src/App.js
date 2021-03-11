456914647832e94328de272f2407b02d765b4873
leopoldkwok <leopoldkwok@gmail.com>
2021-03-11 22:44:44 +0000

    ENV VAR

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
 
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
@@ -13,7 +13,7 @@ function App() {
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=EjgGuJvPbHMV1A8rVDXrDjXmBGvs-ZeU0ImO-1D4R-Q`;
+    url = `${mainUrl}${clientID}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
